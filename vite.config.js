8af6017cf5b14af9c62f7788ace324942edf80d8
YersonLG <yersonlopez847@gmail.com>
2026-03-10 22:37:10 -0500

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuración para GitHub Pages
 export default defineConfig({
   plugins: [react()],
   base: '/Aura1/'
